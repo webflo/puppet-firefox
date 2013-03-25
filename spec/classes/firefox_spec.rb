7c5507e1bffec264b32e2c8c9f30d0409801175c
Florian Weber <f.weber@digi-info.de>
2013-03-25 20:09:08 +0100

    Add specs for other locale settings then 'en-US'.

--- spec/classes/firefox_spec.rb
+++ spec/classes/firefox_spec.rb
@@ -6,4 +6,8 @@ describe 'firefox' do
   it { should contain_package('Firefox').with_provider('appdmg') }
   it { should contain_package('Firefox').with_source('http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/19.0.2/mac/en-US/Firefox%2019.0.2.dmg') }
 
+  context 'with locale => de' do
+    let(:params) { {:locale => 'de'} }
+    it { should contain_package('Firefox').with_source('http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/19.0.2/mac/de/Firefox%2019.0.2.dmg') }
+  end
 end
